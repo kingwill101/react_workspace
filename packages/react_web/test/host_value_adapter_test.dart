ad422e92e08abe6d7d240fa8025b346afd11cfbb
kingwill101 <hey@glenfordwilliams.com>
2026-08-01 17:15:04 -0500

    Test DOM host value decoding

---
+++ packages/react_web/test/host_value_adapter_test.dart
@@ -0,0 +1,25 @@
+@TestOn('browser')
+library;
+
+import 'dart:js_interop';
+
+import 'package:react_js/react_js.dart';
+import 'package:react_web/src/generated/browser_adapter.dart';
+import 'package:react_web/src/generated/html_interfaces.dart';
+import 'package:test/test.dart';
+import 'package:web/web.dart' as web;
+
+void main() {
+  test('decodes a DOM ref as the generated host interface', () {
+    registerBrowserAdapters();
+    final element = web.document.createElement('div');
+
+    final decoded = ReactCodecRegistry.decodeHostValue(
+      'web',
+      'HTMLDivElement',
+      element as JSAny,
+    );
+
+    expect(decoded, isA<HTMLDivElement>());
+  });
+}
